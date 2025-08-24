c96d2a5ef67d3c82f27b38aba806f9d785991bbc
imagefactory-web <factoryimage2@gmail.com>
2025-08-24 16:52:25 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,14 +5,6 @@ terraform {
       version = "~> 5.0"
     }
   }
-
-  backend "s3" {
-    bucket         = "demo-terraform-eks-state-s3-bucket"
-    key            = "terraform.tfstate"
-    region         = "us-west-2"
-    dynamodb_table = "terraform-eks-state-locks"
-    encrypt        = true
-  }
 }
 
 provider "aws" {
